2bfbc8caf8d3cae7f41c167a866cbcded92af27d
Leonardo Santana <leonardo@DESKTOP-RO5QGS8.localdomain>
2020-11-12 00:39:20 -0300

    refatorando testes, criando makeSut

--- src/domain/usecases/movie-usecase.spec.js
+++ src/domain/usecases/movie-usecase.spec.js
@@ -17,31 +17,36 @@ class MovieUseCase {
   }
 }
 
+const makeSut = () => {
+  class LoadMovieByTitleRepositorySpy {
+    async getMovie (title) {
+      this.movie = {}
+      this.movie.title = title
+
+      return this.movie
+    }
+  }
+
+  const loadMovieByTitleRepositorySpy = new LoadMovieByTitleRepositorySpy()
+  const sut = new MovieUseCase(loadMovieByTitleRepositorySpy)
+  return { sut, loadMovieByTitleRepositorySpy }
+}
+
 describe('Movie UseCase Tests', () => {
   test('Should throw if no title provided', async () => {
-    const sut = new MovieUseCase()
+    const { sut } = makeSut()
     const promise = sut.getMovie(null, null)
     await expect(promise).rejects.toThrow(new MissingParamError('title'))
   })
 
   test('Should throw if no language provided', async () => {
-    const sut = new MovieUseCase()
+    const { sut } = makeSut()
     const promise = sut.getMovie('any_title', null)
     await expect(promise).rejects.toThrow()
   })
 
   test('Should getMovie form repository', async () => {
-    class LoadMovieByTitleRepositorySpy {
-      async getMovie (title) {
-        this.movie = {}
-        this.movie.title = title
-
-        return this.movie
-      }
-    }
-
-    const loadMovieByTitleRepositorySpy = new LoadMovieByTitleRepositorySpy()
-    const sut = new MovieUseCase(loadMovieByTitleRepositorySpy)
+    const { sut, loadMovieByTitleRepositorySpy } = makeSut()
     await sut.getMovie('any_title', 'any_language')
     expect(loadMovieByTitleRepositorySpy.movie.title).toBe('any_title')
   })
